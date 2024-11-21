579c041746d70d8b9418505cfe33f7680c529dc9
nichcode <agbonicholas04@gmail.com>
2024-11-21 20:58:25 +0000

    multi window test v1

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -4,13 +4,15 @@
 using namespace prime;
 
 b8 window_test();
+b8 multi_window_test();
 b8 context_test();
 b8 forward_renderer2D_test();
 b8 deferred_renderer2D_test();
 
 int main(int argc, char** argv) 
 {
-	TestManager::add_test(window_test, "window_test");
+	//TestManager::add_test(window_test, "window_test");
+	TestManager::add_test(multi_window_test, "multi_window_test");
 	/*TestManager::add_test(context_test, "context_test");
 	TestManager::add_test(forward_renderer2D_test, "forward_renderer2D_test");
 	TestManager::add_test(deferred_renderer2D_test, "deferred_renderer2D_test");*/
